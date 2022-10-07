11d7686ccacd44ce9f10fd99f6f37876a15bbec7
Damian Kądziela <dkkquality@gmail.com>
2022-10-07 03:18:28 +0200

    desktop footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Wrapper, ContactDetails, CopyrightDetails } from './Footer.styles';
+import { Wrapper, ContactDetails, CopyrightDetails, IconsWrapper } from './Footer.styles';
 import { StyledIcon } from 'components/StyledIcon';
 import { Link } from 'gatsby';
 import Logo from 'assets/icons/logo.svg';
+import { isDesktop } from 'utils/isDesktop';
+import FacebookIcon from 'icons/facebook.svg';
+import TiktokIcon from 'icons/tiktok.svg';
 
 export const Footer = () => {
     return (
         <Wrapper>
-            <StyledIcon color={({ theme }) => theme.color.black} width="90px">
+            <StyledIcon color={({ theme }) => theme.color.black} width={isDesktop() ? '120px' : '90px'}>
                 <Link to="/">
                     <Logo />
                 </Link>
@@ -19,6 +22,20 @@ export const Footer = () => {
                 <br />
                 kontakt@ossolinsky.pl
             </ContactDetails>
+            {isDesktop() && (
+                <IconsWrapper>
+                    <StyledIcon width="24px" color={({ theme }) => theme.color.black}>
+                        <Link to="">
+                            <FacebookIcon />
+                        </Link>
+                    </StyledIcon>
+                    <StyledIcon width="24px" color={({ theme }) => theme.color.black}>
+                        <Link to="">
+                            <TiktokIcon />
+                        </Link>
+                    </StyledIcon>
+                </IconsWrapper>
+            )}
             <CopyrightDetails>
                 @2022 Ossolinsky
                 <br />
--- src/components/Footer/Footer.styles.js
+++ src/components/Footer/Footer.styles.js
@@ -4,12 +4,12 @@ export const Wrapper = styled.footer`
     display: flex;
     align-items: center;
     flex-direction: column;
-    padding: 4rem 0 8rem;
+    padding: 6rem 0 8rem;
     background-color: ${({ theme }) => theme.color.white};
 
     ${({ theme }) => theme.mq.desktop}{
         background-color: ${({ theme }) => theme.color.gray};
-
+        padding: 6rem 0;
     }
 `
 
@@ -19,10 +19,20 @@ export const ContactDetails = styled.address`
     font-size: 1.6rem;
     color: ${({ theme }) => theme.color.black};
     margin: 2rem 0 3rem;
+
+    ${({ theme }) => theme.mq.desktop}{
+        margin: 2rem 0 ;
+    }
 `
 
 export const CopyrightDetails = styled.small`
     text-align: center;
     font-size: 1rem;
     color: ${({ theme }) => theme.color.text};
+`
+
+export const IconsWrapper = styled.div`
+    display: flex;
+    gap: 2rem;
+    margin-bottom: 2rem;
 `
